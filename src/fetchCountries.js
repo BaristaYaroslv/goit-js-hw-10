41e3a6b203ceba283d179b6de501229eae66cac5
Yaroslav <110727630+BaristaYaroslv@users.noreply.github.com>
2023-01-31 18:52:30 +0300

    8

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,11 +1,7 @@
-const url = "https://restcountries.com/v3.1/name";
-
 export default function fetchCountries(name) {
     
-  return fetch(`${url}/${name}?fields=name,capital,population,flags,languages`)
-    .then(res => {
-      return res.json();
-    }).catch(error) => {
-    console.log(error);
-  };
-}
+  return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
+  .then(res => {
+    return res.json();
+  })
+ };
